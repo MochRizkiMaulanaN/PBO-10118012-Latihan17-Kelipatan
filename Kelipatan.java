3e2f4a9cbeeaafeb460bc76d9af0269b9c0d53ed
MochRizkiMaulanaN <55683135+MochRizkiMaulanaN@users.noreply.github.com>
2019-10-10 20:43:08 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatan;
+
+/**
+ * Nama : Moch Rizki Maulana N
+ * Kelas : IF-1/S1/III
+ * NIM : 10118012
+ * Deskripsi Program: Menampilkan Kelipatan 3.5
+ *
+ * @author LENOVO
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float i = 3.5f;
+        while(i <= 35){
+            System.out.println(i);
+            i+=3.5;
+        }
+        System.out.println("( Develoved By : Moch Rizki Maulana N )");
+    }
+    
+}
